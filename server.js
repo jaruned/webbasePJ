d1326633813275bfa52b59f8cf65d877e38299e8
Ball Weera <ballweera.kasetsin@gmail.com>
2018-05-19 19:36:05 +0700

    Remove implementation code

--- server.js
+++ server.js
@@ -3,24 +3,10 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 
 const app = express()
-let contacts = [
-    { name: 'Weera', phoneNumber: '081-8117784' },
-    { name: 'Nuttaluck', phoneNumber: '080-0937060' },
-    { name: 'Fuji', phoneNumber: '090-2201921' }
-]
+let contacts = []
 
 app.use(bodyParser.json())
 app.use(cors())
-
-app.get('/contacts', (req, res) => {
-    res.send(contacts)
-})
-
-app.post('/contacts', (req, res) => {
-    let newContact = req.body
-    contacts.push(newContact)
-    res.status(201).end()
-})
 
 app.listen(3000, () => {
     console.log('API Server started at port 3000')
