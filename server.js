e1772caa24f7e770965efe3f9b8c28b0bf4c179f
Ball Weera <ballweera.kasetsin@gmail.com>
2018-05-19 19:41:00 +0700

    Add TODO section

--- server.js
+++ server.js
@@ -8,6 +8,14 @@ let contacts = []
 app.use(bodyParser.json())
 app.use(cors())
 
+/// TODO: Develop GET /contacts
+
+
+
+/// TODO: Develop POST /contacts
+
+
+
 app.listen(3000, () => {
     console.log('API Server started at port 3000')
 })
