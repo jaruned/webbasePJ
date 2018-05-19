73025ac96d11566ff92a60f923b65b04e23c1d47
Ball Weera <ballweera.kasetsin@gmail.com>
2018-05-19 19:26:06 +0700

    Implement POST /contacts

--- server.js
+++ server.js
@@ -16,6 +16,12 @@ app.get('/contacts', (req, res) => {
     res.send(contacts)
 })
 
+app.post('/contacts', (req, res) => {
+    let newContact = req.body
+    contacts.push(newContact)
+    res.status(201).end()
+})
+
 app.listen(3000, () => {
     console.log('API Server started at port 3000')
 })
