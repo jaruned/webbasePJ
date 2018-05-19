2e54f4460cc15474318ba330a4a7a394f1ecca9a
Ball Weera <ballweera.kasetsin@gmail.com>
2018-05-19 19:17:03 +0700

    Implement GET /contacts API

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+const app = express()
+let contacts = [
+    { name: 'Weera', phoneNumber: '081-8117784' },
+    { name: 'Nuttaluck', phoneNumber: '080-0937060' },
+    { name: 'Fuji', phoneNumber: '090-2201921' }
+]
+
+app.use(bodyParser.json())
+app.use(cors())
+
+app.get('/contacts', (req, res) => {
+    res.send(contacts)
+})
+
+app.listen(3000, () => {
+    console.log('API Server started at port 3000')
+})
