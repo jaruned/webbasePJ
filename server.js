ab1d2d73ae0e2e1b6f0479b6b29e0939972f8f4d
Jaruned Sanyamad <eye-130@hotmail.com>
2018-05-25 01:02:32 +0700

    testtest

--- server.js
+++ server.js
@@ -9,13 +9,18 @@ app.use(bodyParser.json())
 app.use(cors())
 
 /// TODO: Develop GET /contacts
+app.get('/contacts', (req,res) => {
+    res.json(contacts)
 
+})
 
+app.post('/contacts', (req, res) => {
+    let newContact = req.body.contacts.push(newContacts)
+    res.status(201).send(contacts) 
+
+    })
 
 /// TODO: Develop POST /contacts
-
-
-
 app.listen(3000, () => {
     console.log('API Server started at port 3000')
 })
