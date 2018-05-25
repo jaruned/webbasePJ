39a75bcde937c8bf763da74c0e1bf125e3f73d69
Kittiya Maikam <kittiya018@gmail.com>
2018-05-25 23:11:56 +0700

    again

--- buu-web-programming-webapi-project-examination/server.js
+++ buu-web-programming-webapi-project-examination/server.js
@@ -3,18 +3,28 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 
 const app = express()
-let contacts = []
+let contacts = [{
+     name: 'john',
+    phonenumber : '011-1111111'
+}]
 
 app.use(bodyParser.json())
 app.use(cors())
 
 /// TODO: Develop GET /contacts
-'hello'
+app.get('/contacts', (req, res) => {
+    res.json(contacts)
+
+})
 
 
 /// TODO: Develop POST /contacts
 
-
+app.post('/contacts', (req, res) => {
+    let newContact = req.body
+    contacts.push(newContacts)
+    res.status(201).send()
+})
 
 app.listen(3000, () => {
     console.log('API Server started at port 3000')
