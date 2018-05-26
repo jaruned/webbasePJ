0a76fcb31542efdbf9ed022fb8cef4d89f960a57
Jaruned Sanyamad <eye-130@hotmail.com>
2018-05-26 19:12:22 +0700

    testcommit

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const cors = require('cors')
 const app = express()
 let contacts = [
     {
-         name : 'eye',
-         phoneNumber : '088-88888888'
+         name : 'John',
+         phoneNumber : '011-1111111'
     }
 ]
 
@@ -14,10 +14,8 @@ app.use(bodyParser.json())
 app.use(cors())
 
 /// TODO: Develop GET /contacts
-app.get('/contacts', (req,res) => {
-    res.json(contacts)
 
-    })
+
 
 /// TODO: Develop POST /contacts
 app.post('/contacts', (req, res) => {
